e86004b461f836577792555f9cf9e52df5b2cbeb
Ariel LeRoy <arielmatsuileroy@gmail.com>
2018-11-10 00:29:38 +0000

    added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+require 'open-uri'
+require 'faker'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "creating an ingredient #{i.name}"
+end
+
+Ingredient.create(name: "Liam's piss")
